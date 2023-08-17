b73da4ff5435e7d3a0b43cd8940492e4f4827115
Selenophile18 <hattakihajar28@gmail.com>
2023-08-17 14:58:08 +0100

    test

---
+++ module_05/ex03/RobotomyRequestForm.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RobotomyRequestForm.hpp                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hhattaki <hhattaki@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/12 15:56:44 by hhattaki          #+#    #+#             */
+/*   Updated: 2023/08/13 13:42:52 by hhattaki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include "AForm.hpp"
+
+class	RobotomyRequestForm: public AForm
+{
+	std::string	target;
+public:
+	/* Canonical form */
+	RobotomyRequestForm();
+	RobotomyRequestForm(std::string Target);
+	RobotomyRequestForm(RobotomyRequestForm &copy);
+	RobotomyRequestForm	&operator=(RobotomyRequestForm &other);
+	~RobotomyRequestForm();
+	/* getter */
+	const std::string	&getTarget(void) const;
+	/* Additional func */
+	void	execute(Bureaucrat const & executor) const;
+	void	increment(int *i);
+};
