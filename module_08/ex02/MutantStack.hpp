f64ea79db33cfd345ba5f1466bd80b2ae3a5e421
Selenophile18 <hattakihajar28@gmail.com>
2023-09-12 22:08:56 +0100

    M08: ex02 done

--- module_08/ex02/MutantStack.hpp
+++ module_08/ex02/MutantStack.hpp
@@ -4,13 +4,58 @@
 #include <stack>
 
 template <typename T>
-class	MutantStack: public stack
+class MutantStack : public std::stack<T>
 {
 public:
 	/*  Canonical form  */
-	MutantStack();
-	MutantStack(const MutantStack &copy);
-	MutantStack &operator=(const MutantStack &other)
-	~MutantStack();
-	
-}
+	MutantStack(){};
+	MutantStack(const MutantStack &copy){
+		*this = copy;
+		}
+	MutantStack &operator=(const MutantStack &other){
+		*this = other;
+		return *this;
+		}
+	~MutantStack(){};
+	/*  Iterators  */
+	typedef typename std::stack<T>::container_type::iterator	iterator;
+	typedef typename std::stack<T>::container_type::const_iterator	const_iterator;
+	typedef typename std::stack<T>::container_type::reverse_iterator	reverse_iterator;
+	typedef typename std::stack<T>::container_type::const_reverse_iterator	const_reverse_iterator;
+
+	iterator begin()
+	{
+		return this->c.begin();
+	}
+	iterator end()
+	{
+		return this->c.end();
+	}
+
+	const_iterator cbegin()
+	{
+		return this->c.cbegin();
+	}
+	const_iterator cend()
+	{
+		return this->c.cend();
+	}
+
+	reverse_iterator rbegin()
+	{
+		return this->c.rbegin();
+	}
+	reverse_iterator rend()
+	{
+		return this->c.rend();
+	}
+
+	const_reverse_iterator crbegin()
+	{
+		return this->c.crbegin();
+	}
+	const_reverse_iterator crend()
+	{
+		return this->c.crend();
+	}
+};
---
+++ module_08/ex02/main.cpp
@@ -0,0 +1,55 @@
+#include "MutantStack.hpp"
+
+void	func1(void)
+{
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);
+	std::cout << std::endl;
+}
+
+void	func2(void)
+{
+	MutantStack<int> istack;
+
+	for (int i = 0; i < 10; i++)
+		istack.push(i * 2);
+	MutantStack<int>::iterator iter_s = istack.begin();
+	MutantStack<int>::iterator iter_e = istack.end();
+	while (iter_s != iter_e)
+	{
+		std::cout << *iter_s << ". ";
+		iter_s++;
+	}
+	std::cout << std::endl;
+	std::cout << "size: " << istack.size() << std::endl;
+	istack.pop();
+	std::cout << "after poping one element" << std::endl;
+	std::cout << "size: " << istack.size() << std::endl;
+}
+
+int main()
+{
+	std::cout << "****** test1 ******" << std::endl;
+	func1();
+	std::cout << "****** test2 ******" << std::endl;
+	func2();
+}
